d8fb80eab19604908473598b7b4a5e663a2187e7
Larissa <larimscorrea@gmail.com>
2022-07-20 12:36:00 -0300

    lesson almost done

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Esse repositório contém códigos realizados para o curso de Programação JavaScript, dado pela Juventudade Digital da prefeitura da cidade de Fortaleza, Ceará.
---
+++ part one/index.js
@@ -0,0 +1,42 @@
+/*Faça um Programa que peça dois números e imprima o maior deles.*/
+let numberOne = prompt('Digite um número');
+let numberTwo = prompt('Digite um número');
+
+if(numberOne > numberTwo) {
+    alert('O ' + numberOne + ' é maior que o ' + numberTwo);
+} else(numberTwo > numberOne); {
+    alert('O ' + numberTwo + ' é maior que o ' + numberOne);
+}
+
+/*Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo*/
+let value = prompt('Digite um valor');
+
+
+
+/*Faça um Programa que leia três números e mostre o maior deles.*/
+let numberOnee = prompt('Digite um número');
+let numberTwoo = prompt('Digite um número');
+let numberThree = prompt('Digite um número');
+
+if(numberOnee > numberTwoo && numberOne > numberThree) {
+    alert('O ' + numberOnee + ' é maior que o ' + numberTwoo);
+} if(numberTwoo > numberOnee && numberTwo > numberThree) {
+    alert('O ' + numberTwoo + ' é maior que o ' + numberOnee);
+} else(numberThree > numberOnee && numberThree > numberTwoo); {
+    
+}
+
+
+/*Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.*/
+
+/*Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
+Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.*/
+
+/*Faça um Programa que leia três números e mostre o maior deles.*/
+
+/*Faça um Programa que leia três números e mostre o maior e o menor deles.*/
+
+/* Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+A mensagem "Reprovado", se a média for menor do que sete;
+A mensagem "Aprovado com Distinção", se a média for igual a dez.*/
