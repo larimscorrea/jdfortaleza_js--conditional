82bad6082bdc24167019bd0e4fa6a305149225a4
Larissa Corrêa <101229830+larimscorrea@users.noreply.github.com>
2022-07-29 23:17:51 -0300

    Update index.js

--- part two/index.js
+++ part two/index.js
@@ -1,8 +1,25 @@
 /*Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão)*/
-
+let number = 1*prompt('Digite um número);
+if(number != 2, 4, 6, 8, 0) {
+  alert('O número é ímpar')
+} else if(number != 1, 3, 5, 7, 9) {
+  alert('O número é par')
+} else {
+  alert('O número é inválido'); 
+}
 /*Faça um Programa que peça a hora e apresente uma mensagem de "Bom dia", "Boa tarde" ou "Boa noite" conforme a hora informada.*/
+let day = 1*prompt('Digite a hora);
+if(day >= 5 && day <= 12) {
+  alert('Bom dia')
+} else if(day >= 12 && day <= 18) {
+  alert('Boa tarde')
+} else if(day >= 18 && <= [24], [00]) {
+  alert('Boa noite'); 
+} else {
+  alert('Informação inválida'); 
 
 /*Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.*/
+  
 
 /*As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes. 
 Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
